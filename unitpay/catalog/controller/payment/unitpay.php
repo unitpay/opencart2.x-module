61ea54b97469e5d85b4a00991be405d9a8e118a0
m16a1 <a741su@gmail.com>
2017-07-31 12:01:59 +0300

    Добавлены поля customerEmail, customerPhone и cashItems

--- unitpay/catalog/controller/payment/unitpay.php
+++ unitpay/catalog/controller/payment/unitpay.php
@@ -9,6 +9,7 @@ class ControllerPaymentUnitpay extends Controller {
         $this->load->model('checkout/order');
         $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
 
+
         $data['unitpay_login'] = $this->config->get('unitpay_login');
         $data['unitpay_key']= $this->config->get('unitpay_key');
         $data['success_url']= $this->config->get('unitpay_success_url');
@@ -16,34 +17,40 @@ class ControllerPaymentUnitpay extends Controller {
         $data['inv_id'] = $this->session->data['order_id'];
 
         // Комментарий к заказу
-        $data['inv_desc'] = $this->config->get('config_store') . ' ' . $order_info['payment_firstname'] . ' ' . $order_info['payment_address_1'] . ' ' . $order_info['payment_address_2'] . ' ' . $order_info['payment_city'] . ' ' . $order_info['email'];
-
-        // Сумма заказа
-        $rur_code = 'RUB';
-        $rur_order_total = $this->currency->convert($order_info['total'], $order_info['currency_code'], $rur_code);
-        $data['out_summ'] = $this->currency->format($rur_order_total, $rur_code, $order_info['currency_value'], FALSE);
-        $data['out_summ'] = number_format($data['out_summ'], 2, '.', '');
+        $data['inv_desc'] = $this->config->get('config_store') . ' ' . $order_info['payment_firstname'] . ' ' .
+            $order_info['payment_address_1'] . ' ' . $order_info['payment_address_2'] . ' ' .
+            $order_info['payment_city'] . ' ' . $order_info['email'];
 
         $data['action']="https://unitpay.ru/pay/";
 
-        // Кодировка
-        //$data['encoding'] = "utf-8";
+        $customer = $this->getCustomer($order_info['customer_id']);
 
         $data['merchant_url'] = $data['action'] .
-            $data['unitpay_login'] .
-            '?sum='			. $data['out_summ'] .
-            '&account='		. $data['inv_id']	.
-            '&desc='        . $data['inv_desc'] .
-            '&unitpay_login='		. $data['unitpay_login'] .
-            '&resultUrl=' .  $data['success_url'];
+            $data['unitpay_login'] . '?' . http_build_query(array(
+                'sum'           => $order_info['total'],
+                'currency'      => $order_info['currency_code'],
+                'account'       => $data['inv_id'],
+                'desc'          => $data['inv_desc'],
+                'unitpay_login' => $data['unitpay_login'],
+                'resultUrl'     => $data['success_url'],
+                'cashItems'     => $this->getOrderItems(),
+                'customerEmail' => $customer['email'],
+                'customerPhone' => $customer['telephone'],
+                'signature'     => hash('sha256', join('{up}', array(
+                    $data['inv_id'],
+                    $order_info['currency_code'],
+                    $data['inv_desc'],
+                    $order_info['total'],
+                    $data['unitpay_key']
+                )))
+            ));
 
-//tesrt
         $this->id = 'payment';
 
         if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/unitpay.tpl')) {
             return $this->load->view($this->config->get('config_template') . '/template/payment/unitpay.tpl', $data);
         } else {
-            return $this->load->view('default/template/payment/unitpay.tpl', $data);
+            return $this->load->view('payment/unitpay.tpl', $data);
         }
 
     }
@@ -52,7 +59,7 @@ class ControllerPaymentUnitpay extends Controller {
         echo $this->getResult();
     }
 
-    public function getResult(){
+    public function getResult() {
         $request = $_GET;
 
         if (empty($request['method']) || empty($request['params']) || !is_array($request['params'])) {
@@ -63,7 +70,7 @@ class ControllerPaymentUnitpay extends Controller {
         $params = $request['params'];
 
         $this->load->model('checkout/order');
-        $arOrder=$this->model_checkout_order->getOrder($params['account']);
+        $arOrder=$this->model_checkout_order->getOrder($params['unitpayId']);
 
         // Сумма заказа
         $rur_code = 'RUB';
@@ -78,14 +85,14 @@ class ControllerPaymentUnitpay extends Controller {
         }
 
 
-        if ($method == 'check'){
+        if ($method == 'check') {
 
             if (!$arOrder){
                 return $this->getResponseError('Can\'t find order');
             }
 
             if ($params['sum'] != $total_price){
-                return $this->getResponseError('Сумма оплаты в' . $params['sum'] . ' руб. не совпадает с суммой необходимой для оплаты товара' .
+                return $this->getResponseError('Сумма оплаты в ' . $params['sum'] . ' руб. не совпадает с суммой необходимой для оплаты товара' .
                     'стоимостью ' . $total_price . ' руб.');
             }
 
@@ -129,8 +136,14 @@ class ControllerPaymentUnitpay extends Controller {
     public function confirm()
     {
         $this->load->model('checkout/order');
-        if ($this->config->get('unitpay_create_order')=='1') $this->model_checkout_order->addOrderHistory($this->session->data['order_id'], $this->config->get('unitpay_order_status_id_after_create'), '', true);
-        if ($this->config->get('unitpay_cart_reset')=='1') $this->cart->clear();
+        if ($this->config->get('unitpay_create_order')=='1') {
+            $this->model_checkout_order->addOrderHistory(
+                $this->session->data['order_id'], $this->config->get('unitpay_order_status_id_after_create'), '', true);
+        }
+
+        if ($this->config->get('unitpay_cart_reset')=='1') {
+            $this->cart->clear();
+        }
     }
 
 
@@ -171,14 +184,8 @@ class ControllerPaymentUnitpay extends Controller {
         return hash('sha256', $method.$delimiter.join($delimiter, $params).$delimiter.$secretKey);
     }
 
-    private function getMd5Sign($params, $secretKey){
-        ksort($params);
-        unset($params['sign']);
-        return md5(join(null, $params).$secretKey);
-    }
-
     private function check($params){
-        if ($this->model_checkout_order->getOrder($params['account']))
+        if ($this->model_checkout_order->getOrder($params['unitpayId']))
         {
             return true;
         }
@@ -187,12 +194,33 @@ class ControllerPaymentUnitpay extends Controller {
 
     private function pay($params){
         $new_order_status_id = $this->config->get('unitpay_order_status_id_after_pay');
-        $this->model_checkout_order->addOrderHistory($params['account'], $new_order_status_id, 'оплата через UnitPay', true);
+        $this->model_checkout_order->addOrderHistory($params['unitpayId'], $new_order_status_id, 'оплата через UnitPay', true);
     }
 
-    private function error($params){
+    private function error($params) {
         $new_order_status_id = $this->config->get('unitpay_order_status_id_error');
-        $this->model_checkout_order->addOrderHistory($params['account'], $new_order_status_id, 'ошибка при оплате через UnitPay', false);
+        $this->model_checkout_order->addOrderHistory($params['unitpayId'], $new_order_status_id, 'ошибка при оплате через UnitPay', false);
+    }
+
+    private function getOrderItems()
+    {
+        $this->load->model('account/order');
+        $orderProducts = $this->model_account_order->getOrderProducts($this->session->data['order_id']);
+        return base64_encode(
+                    json_encode(
+                        array_map(function ($item) {
+                            return array(
+                                'name'  => $item['name'],
+                                'count' => $item['quantity'],
+                                'price' => $item['price']
+                            );
+                        }, $orderProducts)));
+    }
+
+    private function getCustomer($customerId)
+    {
+        $this->load->model('account/customer');
+        return $this->model_account_customer->getCustomer($customerId);
     }
 }
 ?>
